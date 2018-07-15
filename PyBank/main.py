5401adecf68eaff9d584500924240f230e06d7dc
Rori Macaulay Cooper <rori.m.cooper@gmail.com>
2018-07-14 19:28:31 -0500

    revised file 7.14

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,6 +7,7 @@ import csv
 file_to_load = os.path.join("..", "PyBank","budget_data.csv")
 file_to_save = os.path.join("..", "PyBank", "analysis_data.txt")
 
+title = str("Financial Analysis of PyBank")
 
 prevRevenue = 0
 totalNumMonths = 0
@@ -41,14 +42,16 @@ with open(file_to_load) as raw_data:
         totalRevenue += thisRevenue
 
     AverageRevenue = totalRevenue/totalNumMonths
+    AvgRev2= round(AverageRevenue,2)
+    #print(AvgRev2)
 
 print("Rori Cooper - Unit 3 Assignment")
 print("")
-print("Financial Analysis of PyBank")
+print(f"{title}")
 print("-----------------------------------------------")
 print(f"Total Number of Months: {totalNumMonths} months")
 print(f"Total Revenue: ${totalRevenue}")
-print(f"Average Change: ${AverageRevenue}")
+print(f"Average Change: ${AvgRev2}")
 print(f"Greatest Increase in Revenue: {MonthofGreatestIncrease} ${GreatestIncrease}")
 print(f"Greatest Decrease in Revenue: {MontofGreatestDecrease} ${GreatestDecrease}")
 print("------------------------------------------------")
@@ -56,15 +59,15 @@ print("------------------------------------------------")
 output = (
     f"\nRori Cooper - Unit 3 Assignment\n"
     f"\n\n"
-    f"\nFinancial Analysis\n"
+    f"\n{title}\n"
     f"\n-----------------------------------------------\n"
     f"\nTotal Number of Months: {totalNumMonths} months\n"
     f"\nTotal Revenue: ${totalRevenue}\n"
-    f"\nAverage Change: ${AverageRevenue}\n"
+    f"\nAverage Change: ${AvgRev2}\n"
     f"\nGreatest Increase in Revenue: {MonthofGreatestIncrease} ${GreatestIncrease}\n"
     f"\nGreatest Decrease in Revenue: {MontofGreatestDecrease} ${GreatestDecrease}\n"
     f"\n------------------------------------------------\n")
-print(output)
+#print(output)
 with open(file_to_save, "w") as txt_file:
     txt_file.write(output)
 
